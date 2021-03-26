5c80664c60d0f43007afbf753479e478def23754
DESKTOP-2RGP7AV\Tritri <tristan2003@orange.fr>
2021-03-26 22:12:21 +0100

    Game Commit

---
+++ game.py
@@ -0,0 +1,58 @@
+import pygame
+from Player import Player
+from monster import Monster
+
+class Game:
+
+    def __init__(self):
+        self.is_playing = False
+        self.all_players = pygame.sprite.Group()
+        self.player = Player(self)
+        self.all_players.add(self.player)
+        self.all_monsters = pygame.sprite.Group()
+        self.pressed = {}
+        
+
+    
+    def start(self):
+        self.is_playing = True
+        self.spawn_monster()
+        self.spawn_monster()
+    
+    
+    def game_over(self):
+        self.all_monsters = pygame.sprite.Group()
+        self.player.health = self.player.max_health
+        self.is_playing = False
+    
+    def update(self, screen):
+        screen.blit(self.player.image, self.player.rect)
+
+        self.player.update_health_bar(screen)
+
+        for projectile in self.player.all_projectiles:
+            projectile.move()
+
+
+        for monster in self.all_monsters:
+            monster.forward()
+            monster.update_health_bar(screen)
+
+        self.player.all_projectiles.draw(screen)
+        
+
+        self.all_monsters.draw(screen)
+
+
+        if self.pressed.get(pygame.K_RIGHT) and self.player.rect.x + self.player.rect.width < screen.get_width():
+            self.player.move_right()
+        elif self.pressed.get(pygame.K_LEFT) and self.player.rect.x > 0:
+            self.player.move_left()
+
+
+    def check_collision(self, sprite, group):
+        return pygame.sprite.spritecollide(sprite, group, False, pygame.sprite.collide_mask)
+
+    def spawn_monster(self):
+        monster = Monster(self)
+        self.all_monsters.add(monster)
